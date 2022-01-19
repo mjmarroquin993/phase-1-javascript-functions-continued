dc26e8e1cd6a54bc50466820d577542f58290be8
Josh Marroquin <mj.marroquin993@gmail.com>
2022-01-19 15:21:21 -0800

    commit tree 503b2cc932595cb94830912b0bbe30f76a1ae8a6

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(brown = "go to the office") {
+    return (`This Monday, I will ${brown}.`)
+}
+
+function wrapAdjective(home) {
+    return function(empathic = "special"){
+        return `You are ${home}${empathic}${home}!`
+    }
+}
